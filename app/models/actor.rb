38a75321a845826b315df5e82a7018369755b019
sadiebennett1 <sadiebenn1@gmail.com>
2019-03-04 12:04:22 -0500

    Done.

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,17 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    return "#{first_name} #{last_name}"
+  end
+
+  def list_roles
+    self.shows.collect do |shows|
+      shows.characters.collect do |characters|
+        return "#{characters.name} - #{shows.name}"
+      end
+    end
+  end
+
 end
